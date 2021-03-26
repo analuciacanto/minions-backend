e4fa845d15b4142e4d34e97416d43c633f811bb9
Ana Canto <anacanto@Ana.anacanto.com.br>
2021-03-26 10:35:15 -0300

    Adicionando serviço de emaiil

--- api/service.js
+++ api/service.js
@@ -1,33 +1,56 @@
 import * as uuid from "uuid";
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
+const nodemailer = require("nodemailer");
+var express = require("express");
 
 //POST
 export const registerMinions = handler(async (event) => {
-    const data = JSON.parse(event.body);
-    const params = {
-      TableName: "minions",
-      Item: {      
-        userid: "123", 
-        minionid: uuid.v1(), 
-        name: data.name, 
-        description: data.description, 
-        image: data.image, 
-        price: data.price,
-        createdAt: Date.now(), 
-      },
-    };
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "minions",
+    Item: {
+      userid: "123",
+      minionid: uuid.v1(),
+      name: data.name,
+      description: data.description,
+      image: data.image,
+      price: data.price,
+      createdAt: Date.now(),
+    },
+  };
   await dynamoDb.put(params);
-
   return params.Item;
-  
+});
+
+export const createPurchase = handler(async (event) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "minionsPurchase",
+    Item: {
+      userid: "123",
+      purchaseid: uuid.v1(),
+      name: data.name,
+      phone: data.phone,
+      amount: data.amount,
+      email: data.email,
+      minion: data.minion,
+      createdAt: Date.now(),
+    },
+  };
+  await dynamoDb.put(params);
+  return params.Item;
+});
+
+export const sendMailPurchase = handler(async () => {
+  return "sucesso";
 });
 
 //GET
 
 export const getMinion = handler(async (event) => {
   const params = {
-    TableName:"minions",
+    TableName: "minions",
     Key: {
       minionid: event.pathParameters.id, // The id of the note from the path
     },
@@ -40,15 +63,14 @@ export const getMinion = handler(async (event) => {
   return result.Item;
 });
 
-
 export const getMinions = handler(async () => {
   const params = {
-    TableName: "minions",  
+    TableName: "minions",
     KeyConditionExpression: "userid = :userid",
     ExpressionAttributeValues: {
       ":userid": "123",
-      },    
-    };
+    },
+  };
 
   const result = await dynamoDb.query(params);
   return result.Items;
